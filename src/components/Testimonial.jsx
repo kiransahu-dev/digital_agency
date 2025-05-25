36d94a1215e22f436421d0833232a3ca21438b4e
kiransahu-dev <sahukiran98.ks@gmail.com>
2025-05-25 16:12:10 +0530

    testimonial completed

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -7,30 +7,34 @@ import ct4 from "../assets/ct-4.jpg";
 const TestimonialData = [
   {
     id: 1,
-    name: "Hrushikesh Sahu (Akshara Enterprises)",
-    text: "Aries Digitals transformed our furniture store from a ghost town into a thriving footfall location. Their strategies are creative, consistent, and spot-on for our audience.",
+    name: "Dr. Hrushikesh Sahu",
+    role: "Sri Ram Hospital",
+    text: "Aries Digitals transformed our online presence completely. From content planning to ad campaigns, everything was executed with strategy and clarity. We saw a 4x increase in leads within 3 months!",
     img: ct3,
     delay: 0.2,
   },
   {
     id: 2,
-    name: "Sivaram Pradhan (Yosoda Nursing Home)",
-    text: "We needed a corporate infographic video for promotion, and Aries Digitals nailed it. The storytelling, visuals, and editing were world-class",
+    name: "Sivaram Pradhan",
+    role: "Yosoda Nursing Home",
+    text: "We needed a corporate infographic video for promotion, and Aries Digitals nailed it. The storytelling, visuals and editing were world-class.",
     img: ct2,
     delay: 0.5,
   },
   {
     id: 3,
-    name: "Manoranjan Sahu (Sri-Ram Hospital)",
-    text: "Aries Digitals transformed our online presence completely. From content planning to ad campaigns, everything was executed with strategy and clarity. We saw a 4x increase in leads within 3 months!",
+    name: "Jagadish Sahu",
+    role: "Akshara Enterprises",
+    text: "Aries Digitals transformed our furniture store from a ghost town into a thriving footfall location. Their strategies are creative, consistent and spot-on for our audience.",
     img: ct4,
     delay: 0.8,
   },
   {
     id: 4,
-    name: "Jagadish Sahu (Mabello Doors & Furniture Pvt. Ltd)",
-    text: "The product shoots by Aries Digitals elevated our brand instantly. Clean, professional, and perfectly lit — exactly what our catalog needed.",
-    img: "https://picsum.photos/102/102",
+    name: "Jagadish Sahu",
+    role: "Mabello Doors & Furniture Pvt. Ltd",
+    text: "The product shoots by Aries Digitals elevated our brand instantly. Clean, professional, and perfectly lit exactly what our catalog needed.",
+    img: ct4,
     delay: 0.8,
   },
 ];
@@ -79,8 +83,8 @@ const Testimonial = () => {
     >
       <div className="container">
         {/* header sec */}
-        <div className="space-y-4 p-6 text-center max-w-[600px] mx-auto mb-6">
-          <h1 className="uppercase font-semibold text-[#a61040] text-3xl">
+        <div className="space-y-4 p-6 text-center max-w-[600px] mx-auto md:mb-6">
+          <h1 className=" font-semibold text-[#a61040] md:text-4xl text-2xl tracking-wide">
             Client Testimonial
           </h1>
           {/* <p className="font-semibold text-3xl">What Our Client Say About Us</p> */}
@@ -90,7 +94,7 @@ const Testimonial = () => {
           <Slider {...setting}>
             {TestimonialData.map((item) => (
               <div key={item.id} className="px-2 h-full">
-                <div className="flex flex-col justify-between h-full min-h-[300px] max-w-3xl mx-auto p-6 md:p-8 rounded-2xl shadow-xl bg-[#b74c6e] text-white transition-all duration-300 hover:shadow-2xl">
+                <div className="flex flex-col justify-between h-full min-h-[250px] max-w-3xl mx-auto p-6 md:p-8 rounded-2xl shadow-xl bg-[#700d28] text-white transition-all duration-300 hover:shadow-2xl">
                   {/* Top */}
                   <div className="flex items-center gap-4">
                     <img
@@ -101,13 +105,13 @@ const Testimonial = () => {
                     <div>
                       <p className="text-lg font-semibold">{item.name}</p>
                       {item.role && (
-                        <p className="text-sm text-white/70">{item.role}</p>
+                        <p className="text-md  text-white/80">{item.role}</p>
                       )}
                     </div>
                   </div>
 
                   {/* Bottom */}
-                  <div className="mt-6">
+                  <div className="mt-4">
                     <p className="text-sm leading-relaxed tracking-wide min-h-[100px]">
                       {item.text}
                     </p>
