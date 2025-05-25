90cb7841f5d1cd716fb16990fd079d37a3671405
kiransahu-dev <sahukiran98.ks@gmail.com>
2025-05-25 15:40:05 +0530

    services completed

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import {
-  FaShareAlt,
-  FaGoogle,
+  FaInstagram,
   FaLaptopCode,
   FaPaintBrush,
   FaUserFriends,
   FaCameraRetro,
 } from "react-icons/fa";
+import { FaMeta } from "react-icons/fa6";
 import { slideIn, staggerContainer } from "../utils/motion";
 import { motion } from "framer-motion";
 
@@ -15,7 +15,7 @@ const Services = () => {
     <>
       <section className="container mx-auto md:pt-32 pt-10 px-4" id="services">
         <div className="flex items-center justify-center">
-          <h1 className="font-bold text-[#a61040] text-4xl">Services</h1>
+          <h1 className="font-bold text-[#a61040] text-4xl tracking-wide">Services</h1>
         </div>
         <motion.div
           variants={staggerContainer(0.2, 0.1)}
@@ -28,14 +28,13 @@ const Services = () => {
             {/* Shine animation overlay */}
             <div className="absolute top-0 left-[-100%] w-[100%] h-full bg-gradient-to-r from-transparent via-red-600/75 to-transparent transform rotate-[25deg] blur-sm opacity-0 transition-all duration-1000 group-hover:left-[100%] group-hover:opacity-100"></div>
             <div className="relative z-10 p-4 text-white">
-              <FaShareAlt size={"35px"} color="#a61040" className="pb-3" />
+              <FaInstagram size={"35px"} color="#a61040" className="pb-3" />
               <h4 className="font-semibold text-lg pb-3">
                 Social Media Marketing
               </h4>
               <p className="font-stretch-50% text-sm text-white/70">
                 Create scroll-stopping content, boost engagement, grow your
-                community, build your brand, enga className="font-medium
-                text-sm"ge your audience, and generate leads across platforms
+                community, build your brand, engage your audience, and generate leads across platforms
                 like Instagram, Facebook, and LinkedIn.
               </p>
             </div>
@@ -51,7 +50,7 @@ const Services = () => {
               <p className="font-stretch-50% text-sm text-white/70">
                 We connect brands with the right influencers to amplify your
                 message, build authentic relationships with your audience, and
-                drive conversions like never before
+                drive conversions like never before.
               </p>
             </div>
           </div>
@@ -59,14 +58,14 @@ const Services = () => {
             {/* Shine animation overlay */}
             <div className="absolute top-0 left-[-100%] w-[100%] h-full bg-gradient-to-r from-transparent via-red-600/75 to-transparent transform rotate-[25deg] blur-sm opacity-0 transition-all duration-1000 group-hover:left-[100%] group-hover:opacity-100"></div>
             <div className="relative z-10 p-4 text-white ">
-              <FaGoogle size={"35px"} color="#a61040" className="pb-3" />
+              <FaMeta size={"35px"} color="#a61040" className="pb-3" />
               <h4 className="font-semibold text-lg pb-3">
-                Google Ads and Meta Ads campaigns
+                Meta and Google Ad campaigns
               </h4>
               <p className="font-stretch-50% text-sm text-white/70">
                 Get immediate results with high-performing paid campaigns
                 designed to maximize ROI. Reach your ideal customer faster with
-                high-converting PPC and display ad campaigns.
+                high-converting PPC, lead generation and display ad campaigns.
               </p>
             </div>
           </div>
@@ -91,11 +90,11 @@ const Services = () => {
             <div className="relative z-10 p-4 text-white">
               <FaPaintBrush size={35} color="#a61040" className="pb-3" />
               <h4 className="font-semibold text-lg pb-3">
-                Logo Design and Branding
+                  Branding and Graphic Designing
               </h4>
               <p className="font-stretch-50% text-sm text-white/70">
                 From logos to full brand identity — we make sure your brand
-                leaves a lasting and unforgettable impression
+                leaves a lasting and unforgettable impression.
               </p>
             </div>
           </div>
@@ -109,7 +108,7 @@ const Services = () => {
               </h4>
               <p className="font-stretch-50% text-sm text-white/70">
                 This brings your brand to life and boosts your conversion power.
-                Whether you're launching a new product, updating your website,
+                Whether you're launching a new product, updating your website
                 or telling your brand story — our visuals are crafted to
                 connect, engage, and convert.
               </p>
